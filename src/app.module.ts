2f5cd4a18880c4997af8dadc5ea46761c16ce80a
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2025-04-21 19:33:41 +0800

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BuildModule } from './build/build.module';
 import { BuildService } from './build/build.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BuildModule],
+  imports: [BuildModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, BuildService],
 })
